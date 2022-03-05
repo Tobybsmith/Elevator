54f4ff690fdbf89b8828ef6f72bd736e53066afd
Tobybsmith <tobybsmith121@gmail.com>
2022-03-05 13:42:29 -0500

    updated level system

--- scenes/objects/levels/EntranceFlag.gd
+++ scenes/objects/levels/EntranceFlag.gd
@@ -1,30 +1,30 @@
+#baselevel is before the elevator entrance,
+#you hit the Area2D and it triggers the game to start
+#then it instances Levels, which are made of randomly connected level bits
+#one floor = one level made of a ton of level bits
+#after clearing the floor, there is a new elevator to take upwards
+#or maybe sideways, or even downwards
+
 extends Area2D
 
-var level1 = load("res://scenes/objects/levels/1.tscn")
-var level2 = load("res://scenes/objects/levels/2.tscn")
+var level = load("res://scenes/objects/levels/level.tscn")
 var l
+var levels
 var enter = false
 var canInstance = true
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	randomize()
-	var levelno = randi() % 2
-	var level_array = [level1, level2]
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	#only make one
+	#instances a level, which is built from bits in level.gd
+	#this means that levels can be made and instanced into the scene separatly
 	if(enter and canInstance):
-		print("Instanced")
+		print("Instancing")
 		not enter
 		canInstance = false
-		#need to have a base level (like train) that gets created with instances of little bits of levels
-		l = level1.instance()
+		l = level.instance()
 		get_parent().add_child(l)
 		l.set_position(get_parent().get_position() + Vector2(2064, 0))
-		
 
 func _on_EntranceFlag_body_entered(body):
 	if(body.name == "Player"):
---
+++ scenes/objects/levels/level.gd
@@ -0,0 +1,24 @@
+#baselevel is before the elevator entrance,
+#you hit the Area2D and it triggers the game to start
+#then it instances Levels, which are made of randomly connected level bits
+#one floor = one level made of a ton of level bits
+#after clearing the floor, there is a new elevator to take upwards
+#or maybe sideways, or even downwards
+
+extends Node2D
+
+var level1 = load("res://scenes/objects/levels/bits/1.tscn")
+var level2 = load("res://scenes/objects/levels/bits/2.tscn")
+var level3 = load("res://scenes/objects/levels/bits/3.tscn")
+var l
+var levels
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var level_array = [level1, level2, level3]
+	for i in range(5):
+		randomize()
+		l = level_array[randi()%3].instance()
+		get_parent().add_child(l)
+		#need to get the position just right, but this does work
+		l.set_position(get_parent().get_position() + Vector2(2064 + 1000 * i, 0))
