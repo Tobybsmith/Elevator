88ad6866c5af4b99d754ed2e6dec47606fb0e91b
Tobybsmith <tobybsmith121@gmail.com>
2022-03-07 17:42:30 -0500

    just a little more commenting

--- Root.gd
+++ Root.gd
@@ -4,6 +4,7 @@ extends Node2D
 #elevatorarea is given, and destroys the old terrain and adds new terrain using the 
 #level construction code from the baselevel.gd file
 
+#load the level(there is only one that gets destroyed and recreated with each elevator teleport)
 var l = load("res://scenes/objects/levels/level.tscn")
 
 var prep = false
@@ -18,15 +19,11 @@ func _process(delta):
 	pass
 
 func _prepare_area():
+	#only prepare the level once
 	if(not prep):
 		prep = true
-		#here needs to be the code to clear this node of terrain
-		#there will probably be one child node called "terrain"
-		#and then it must create a new child node called "terrain"
-		#with new instanced terrain 
-		#deletes the current terrain
+		#this code removes the "level" node from the "Root" node
 		var count = self.get_child_count()
-		#MINKUS ??? should remove the terrain node
 		for i in range(count):
 			if(self.get_child(i).name == "level"):
 				self.remove_child(self.get_child(i))
@@ -36,7 +33,7 @@ func _prepare_area():
 		level.name = "level"
 		self.add_child(level)
 		connect("make_level", get_node("level"), "make_level")
-		#make the level node make a level
+		#make the level node make a level - will need to pass information like type and length
 		emit_signal("make_level")
 		level.set_global_position(Vector2.ZERO)
 		#teleport player back to the level begin
